fc2e164d7e25499ca37f1134846c09a7e3179979
Stoychev <stoychev.nas@gmail.com>
2024-07-15 16:07:01 +0300

    New settings and add notifications

--- screens/Home.js
+++ screens/Home.js
@@ -1,15 +1,55 @@
-import { View, ImageBackground, StyleSheet } from "react-native";
+import { useState, useEffect } from "react";
+import { View, Button, ImageBackground, StyleSheet } from "react-native";
 import HomePageButtons from "../components/UI/HomePageButtons";
+import * as Notifications from 'expo-notifications';
 
 import { useNavigation } from "@react-navigation/native";
 
 const image = '../assets/backgrounds/One.jpg';
 
+// NOTIFICATIONS
+Notifications.setNotificationHandler({
+    handleNotification: async () => {
+        return {
+            shouldPlaySound: false,
+            shouldSetBadge: false,
+            shouldShowAlert: true
+        }
+    }
+});
+// NOTIFICATIONS
+
 function Home() {
     const navigation = useNavigation();
 
+    // NOTIFICATIONS
+    useEffect(() => {
+        const subscription = Notifications.addNotificationReceivedListener((notification) => {
+            console.log('NOTIFICATION RECEIVED');
+            console.log(notification);
+            const userName = notification.request.content.data.userName;
+            console.log(userName);
+        });
+        return () => {
+            subscription.remove();
+        }
+    }, [])
+    function scheduleNotificationHandler() {
+        Notifications.scheduleNotificationAsync({
+            content: {
+                title: 'My first local notification',
+                body: 'This is the body of the notification.',
+                data: { userName: 'Max' }
+            },
+            trigger: {
+                seconds: 5
+            }
+        })
+    }
+    // NOTIFICATIONS
+
     function goToScreen(screen) {
-        navigation.navigate(screen, {newValue: 'add'})
+        navigation.navigate(screen, { newValue: 'add' })
     }
 
     return (
@@ -31,6 +71,11 @@ function Home() {
                         onPress={() => goToScreen('AllTasks')}
                     />
                 </View>
+                {/* NOTIFICATION */}
+
+                <Button title="Schedule Notification" onPress={scheduleNotificationHandler} />
+
+                {/* NOTIFICATION */}
             </ImageBackground>
         </View>
     )
